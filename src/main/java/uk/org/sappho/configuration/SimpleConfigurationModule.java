08437e5e30af58644fabb988cc453abe1e6e7dc7
Andrew Heald <andrew@heald.co.uk>
2011-01-24 14:01:32 +0000

    CHM-55: Load modules not classes.

--- src/main/java/uk/org/sappho/configuration/SimpleConfigurationModule.java
+++ src/main/java/uk/org/sappho/configuration/SimpleConfigurationModule.java
@@ -4,8 +4,18 @@ import com.google.inject.AbstractModule;
 
 public class SimpleConfigurationModule extends AbstractModule {
 
+    private final Configuration config;
+
+    public SimpleConfigurationModule() {
+        config = new SimpleConfiguration();
+    }
+
+    public SimpleConfigurationModule(Configuration config) {
+        this.config = config;
+    }
+
     @Override
     protected void configure() {
-        bind(Configuration.class).to(SimpleConfiguration.class);
+        bind(Configuration.class).toInstance(config);
     }
 }
